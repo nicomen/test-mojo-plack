b17393f9a02309f3108fdc7e7223af7064226bda
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2018-06-12 11:07:11 +0200

    Bump version

--- lib/Test/Mojo/Plack.pm
+++ lib/Test/Mojo/Plack.pm
@@ -145,11 +145,11 @@ Test::Mojo::Plack - Test Plack-compatible applications with Test:Mojo
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =cut
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 =head1 SYNOPSIS
 

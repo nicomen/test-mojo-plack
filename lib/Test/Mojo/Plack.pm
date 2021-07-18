97ed18fc5a4b8e4dcacb6914dc684a438677839b
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2021-07-18 15:57:54 +0200

    Release 0.12

--- lib/Test/Mojo/Plack.pm
+++ lib/Test/Mojo/Plack.pm
@@ -147,11 +147,11 @@ Test::Mojo::Plack - Test Plack-compatible applications with Test:Mojo
 
 =head1 VERSION
 
-Version 0.10
+Version 0.12
 
 =cut
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 =head1 SYNOPSIS
 
